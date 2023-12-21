2c9e25ee8d9a52f2d137b3a8ad567c079ce9c12f
MelcherMate <melchermate28@gmail.com>
2023-12-21 12:34:42 -0600

    finalise git_usage file

--- tutorial/git_usage.py
+++ tutorial/git_usage.py
@@ -14,13 +14,10 @@ Git has 2 flows:
 - One is when we hav a git operated code:
     - once I created a stage, i can commit ti
     - i will do and can do the commit
-    - I create a remote branch on github and push my ready code
+    - I create a remote branch on github and push my rea
 
-
-    - first you create a change on your code (can be only 1 new caracter or line as well)
-    - I would watch these changes: "git add"  ->  
-
-
-
-
+    
+    
+first you create a change on your code (can be only 1 new caracter or line as well)
+I would watch these changes: "git add"  ->  
 """
